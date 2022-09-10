1817111ee19b7bb75c3309ee0fc5785e83ce9423
SydneySmoothvile <69866777+SydneySmoothvile@users.noreply.github.com>
2022-09-10 23:39:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-m3epc2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-m3epc2)
---
+++ src/components/XForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>XForm template</div>
+
+  <div>
+    <label> Name </label>
+    <input v-model="fields.myfields1" />
+    <form>
+      <input v-model="myObject1.name" type="text" placeholder="Name" />
+    </form>
+  </div>
+
+  <div>
+    <label> Date of Birth </label>
+    <input type="datetime" placeholder="Input" />
+  </div>
+
+  <div>
+    <label>Eye color </label>
+  </div>
+
+  <div name="id"></div>
+
+  <div>
+    <label> Created </label>
+    <input name="created" type="datetime" placeholder="Input" />
+  </div>
+
+  <div>
+    <label> Updated </label>
+    <input name="updated" type="datetime" placeholder="Input" />
+  </div>
+
+  <div>
+    <label> Label </label>
+    <input name="label" type="text" placeholder="Input" />
+  </div>
+
+  <div>
+    <label> Name </label>
+    <input name="name" type="text" placeholder="Input" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    propsA:['myObject1', 'myfields1'],
+    propsB:['myObject2', 'myfields2'],
+    propsC:['myObject3', 'myfields3']
+  }
+};
+</script>
