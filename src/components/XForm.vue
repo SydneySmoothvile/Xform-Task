23a119b9be1d7e649581ec7b8a02a2a672fb4c45
SydneySmoothvile <69866777+SydneySmoothvile@users.noreply.github.com>
2022-09-21 14:56:39 +0300

    1 file modified

--- src/components/XForm.vue
+++ src/components/XForm.vue
@@ -1,14 +1,35 @@
 <template>
   <div>XForm template</div>
   <div>
-    <h1 v-for="field in fields" :key="field.field" >
-      {{field.component}}
-    </h1>
+    <input
+      v-for="field in fields"
+      :key="fields.field"
+      v-bind:fields="fields.field"
+    />
+
+    <div>
+      My Input:
+      <input v-model="inputVal" />
+    </div>
+
+    {{ field.component }}
   </div>
+
+  <input type="text" :value="myObject1" />
 </template>
 
 <script>
 export default {
-  props: ['fields']
+  props: ['fields'],
+  computed: {
+    inputVal: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      },
+    },
+  },
 };
 </script>
